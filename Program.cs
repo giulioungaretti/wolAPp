3e7e695c97e67b281a4eec6c4751bee2e8a13129
Giulio Ungaretti <giungare@microsoft.com>
2020-10-31 10:07:46 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wolAPp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
